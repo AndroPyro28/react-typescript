5a387b0645a4f319591997b44efa10065f7ae98f
andropyro28 <menandroeugenio1028@gmail.com>
2022-06-25 04:18:24 -0400

    restricting props with 'never'

--- src/App.tsx
+++ src/App.tsx
@@ -6,52 +6,24 @@ import Greet from "./components/Greet";
 import Heading from "./components/Heading";
 import Input from "./components/Input";
 import Oscar from "./components/Oscar";
-import Counter from "./state/Counter"
-import CounterClass from "./class/Counter"
+import Counter from "./state/Counter";
+import CounterClass from "./class/Counter";
 import Status from "./components/Status";
 import LoggedIn from "./state/LoggedIn";
 import User from "./state/User";
-import {ThemeContextProvider} from './context/ThemeContext';
-import Box from './context/Box';
+import { ThemeContextProvider } from "./context/ThemeContext";
+import Box from "./context/Box";
 import Private from "./auth/Private";
 import { Profile } from "./auth/Profile";
 import { Routes, Route, useLocation } from "react-router-dom";
 import List from "./generics/List";
+import RandomNumber from "./restriction/RandomNumber";
 function App() {
-
   return (
     <div className="App">
-      <List 
-        items={['andro', 'jean', 'john', 'maze']}
-        onClick={(item) => console.log(item)}
-      />
-
-      <List 
-        items={[1,2,3,4,5]}
-        onClick={(item) => console.log(item)}
-      />
-
-        <List 
-        items={[
-          {
-            id: 1,
-            firstname: 'andro',
-            lastname: 'eugenio'
-          },
-          {
-            id: 2,
-              firstname: 'jean',
-              lastname: 'correa'
-          },
-          {
-            id: 3,
-            firstname: 'john',
-            lastname: 'doe'
-          }
-      ]}
-        onClick={(item) => console.log(item)}
-      />
-
+      <RandomNumber value={-1} isNegative />
+      <RandomNumber value={0} isZero />
+      <RandomNumber value={1} isPositive />
     </div>
   );
 }
---
+++ src/restriction/RandomNumber.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+interface RandomNumberType {
+  value: number;
+}
+
+type RandomNumberPositivetype = RandomNumberType & {
+  isPositive?: boolean;
+  isNegative?: never;
+  isZero?: never;
+};
+
+type RandomNumberNegativetype = RandomNumberType & {
+  isPositive?: never;
+  isNegative?: boolean;
+  isZero?: never;
+};
+
+type RandomNumberZerotype = RandomNumberType & {
+  isPositive?: never;
+  isNegative?: never;
+  isZero?: boolean;
+};
+
+type randomNumberProps =
+  | RandomNumberPositivetype
+  | RandomNumberNegativetype
+  | RandomNumberZerotype;
+
+function RandomNumber({
+  value,
+  isPositive = false,
+  isNegative = false,
+  isZero = false,
+}: randomNumberProps) {
+  return (
+    <div>
+      {value}
+      {isPositive && "is positive number"}
+      {isNegative && "is negative number"}
+      {isZero && "is zero number"}
+    </div>
+  );
+}
+
+export default RandomNumber;
